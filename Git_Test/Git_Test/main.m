6a847ddb4c321996b9e7522e27e99dc8f47ceae7
TH <lisktic@126.com>
2016-03-25 16:02:47 +0800

    0.1

---
+++ Git_Test/Git_Test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Git_Test
+//
+//  Created by lanou3g on 16/3/25.
+//  Copyright © 2016年 hyc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
